e85f7a12e601eba2b2cf26e8cf18f4b20ddddf40
Sadeed <msadeedashraf@gmail.com>
2024-04-22 00:03:44 -0400

    Add project files.

---
+++ JobZila/Pages/JobSearch.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JobZila.Pages
+{
+    public class JobSearchModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ JobZila/Pages/TermOfService.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JobZila.Pages
+{
+    public class TermOfServiceModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
