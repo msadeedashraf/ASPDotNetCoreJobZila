281e3a28d7ed6441cf23307a623514bc2aab0846
Sadeed <msadeedashraf@gmail.com>
2024-04-23 10:47:38 -0400

    add job form validation done

---
+++ JobZila/Pages/AddJob.cshtml.cs
@@ -0,0 +1,57 @@
+using JobZila.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace JobZila.Pages
+{
+    public class AddJobModel : PageModel
+    {
+        [BindProperty]
+        public string Title { get; set; }
+        [BindProperty]
+        public string Company { get; set; }
+        [BindProperty]
+        public string? Description { get; set; }
+        [BindProperty]
+        public string City { get; set; }
+
+
+
+        //[BindProperty]
+        // public Jobs jobs { get; set; }
+
+
+
+
+
+        public void OnGet()
+        {
+            string abc = "Sadeed";
+            return abc;
+
+            
+        }
+
+        public IActionResult OnPost()
+        {
+            string myValues = $"{Title} - {Company} - {Description} - {City}";
+
+            //string myValues = $"{jobs.title} - {jobs.company} - {jobs.description} - {jobs.city}";
+
+
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            
+            
+            
+            
+            return Redirect("JobListing");
+
+            
+        }
+
+       
+    }
+}
